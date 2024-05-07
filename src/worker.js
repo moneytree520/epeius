332504dd709c73e52ea2ff88d59075b3d54e91c0
moneytree520 <77653761+moneytree520@users.noreply.github.com>
2024-05-07 16:04:13 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,6 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
-let sha224Password = '08f32643dbdacf81d0d511f1ee24b06de759e90f8edf742bbdc57d88';
+let sha224Password = 'e9a3edf86794b6dea4659e8ab3afd96380f5748c7dbba97c1a5205a2';
 let proxyIP = ["workers.cloudflare.cyou"];
 
 if (!isValidSHA224(sha224Password)) {
